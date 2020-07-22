1704cd661928f89c3ac68cd85fef6d286d5204a9
woowonjin <45147364+woowonjin@users.noreply.github.com>
2020-07-22 15:24:59 +0900

    달력 기본기능은 구현했고 요일에 왜 opacity가 들어갈까 ? #6

---
+++ CocoNavi/CalendarTest.swift
@@ -0,0 +1,11 @@
+//
+//  CalendarTest.swift
+//  CocoNavi
+//
+//  Created by 우원진 on 2020/07/22.
+//  Copyright © 2020 WooWonJin. All rights reserved.
+//
+
+import Foundation
+
+
--- CocoNavi/CalendarViewController.swift
+++ CocoNavi/CalendarViewController.swift
@@ -8,23 +8,241 @@
 
 import UIKit
 
-class CalendarViewController: UIViewController {
+class CalendarCell: UICollectionViewCell {
+    @IBOutlet weak var dateLabel: UILabel!
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class CalendarViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var CalendarCollectionView: UICollectionView!
+    
+    var numOfDaysInMonth : [Int] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    var currentYear : Int = -1
+    var currentMonth : Int = -1
+    var todayDate : Int = -1
+    
+    func getDayOfWeek(_ today:String) -> Int? {
+        let formatter  = DateFormatter()    // 1
+        formatter.dateFormat = "yyyy-MM-dd" // 2
+        guard let todayDate = formatter.date(from: today) else { return nil }  // 3
+        let myCalendar = Calendar(identifier: .gregorian)   // 4
+        let weekDay = myCalendar.component(.weekday, from: todayDate) // 5
+        return weekDay
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.currentYear = Calendar.current.component(.year, from: Date())
+        self.currentMonth = Calendar.current.component(.month, from: Date())
+        self.todayDate = Calendar.current.component(.day, from: Date())
+        if(currentYear % 4 == 0){
+            numOfDaysInMonth[1] = 29
+        }
+        else{
+            numOfDaysInMonth[1] = 28
+        }
+        print("viewDidLoad!!")
+        self.CalendarCollectionView.delegate = self
+        self.CalendarCollectionView.dataSource = self
+//        self.CalendarCollectionView.reloadData()
+        self.yearLabel.text = "\(self.currentYear)"
+        self.monthLabel.text = "\(self.currentMonth)월"
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 49
+    }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = CalendarCollectionView.dequeueReusableCell(withReuseIdentifier: "CalendarCell", for: indexPath) as! CalendarCell
+        cell.dateLabel.translatesAutoresizingMaskIntoConstraints = true
+//        cell.dateLabel.topAnchor.constraint(equalTo: cell.topAnchor, constant: 0)
+//        cell.dateLabel.leftAnchor.constraint(equalTo: cell.leftAnchor, constant: 0)
+//        cell.dateLabel.rightAnchor.constraint(equalTo: cell.rightAnchor, constant: 0)
+        cell.dateLabel.frame = CGRect(x: 0, y: 0, width: cell.frame.size.width, height: cell.frame.size.width/2)
+        if(indexPath.row < 7){
+            switch(indexPath.row){
+                case 0:
+                    cell.dateLabel.text = "일"
+                    cell.dateLabel.textColor = .red
+                case 1:
+                    cell.dateLabel.text = "월"
+                    cell.dateLabel.textColor = .black
+                case 2:
+                    cell.dateLabel.text = "화"
+                    cell.dateLabel.textColor = .black
+                case 3:
+                    cell.dateLabel.text = "수"
+                    cell.dateLabel.textColor = .black
+                case 4:
+                    cell.dateLabel.text = "목"
+                    cell.dateLabel.textColor = .black
+                case 5:
+                    cell.dateLabel.text = "금"
+                    cell.dateLabel.textColor = .black
+                case 6:
+                    cell.dateLabel.text = "토"
+                    cell.dateLabel.textColor = .black
+                default:
+                    cell.dateLabel.text = "\(indexPath.row)"
+                    cell.dateLabel.textColor = .black
+                }
+            cell.dateLabel.font = UIFont.boldSystemFont(ofSize: 12.0)
+            print(cell.dateLabel.text)
+        }
+        else{
+            let firstDay = self.getDayOfWeek("\(self.currentYear)-\(self.currentMonth)-01")!
+            if((indexPath.row-6) >= firstDay && (indexPath.row-6) <= self.numOfDaysInMonth[self.currentMonth-1]+firstDay-1){
+                switch(indexPath.row % 7){
+                case 0:
+                    cell.dateLabel.text = "\(indexPath.row-5-firstDay)"
+                    cell.dateLabel.textColor = .red
+                case 6:
+                    cell.dateLabel.text = "\(indexPath.row-5-firstDay)"
+                    cell.dateLabel.textColor = .blue
+                default:
+                    cell.dateLabel.text = "\(indexPath.row-5-firstDay)"
+                    cell.dateLabel.textColor = .black
+                }
+                cell.dateLabel.font = UIFont.boldSystemFont(ofSize: 10.0)
+                cell.dateLabel.layer.opacity = 0.8
+            }
+            else if(indexPath.row-6 < firstDay){
+                var previousYear = -1
+                var previousMonth = -1
+                if(self.currentMonth == 1){
+                    previousYear = self.currentYear-1
+                    previousMonth = 12
+                }
+                else{
+                    previousYear = self.currentYear
+                    previousMonth = self.currentMonth-1
+                }
+                if(previousYear % 4 == 0){
+                    numOfDaysInMonth[1] = 29
+                }
+                else{
+                    numOfDaysInMonth[1] = 28
+                }
+                switch(indexPath.row % 7){
+                case 0:
+                    cell.dateLabel.text = "\(numOfDaysInMonth[previousMonth-1]-firstDay+indexPath.row-5)"
+                    cell.dateLabel.textColor = .red
+                case 6:
+                    cell.dateLabel.text = "\(numOfDaysInMonth[previousMonth-1]-firstDay+indexPath.row-5)"
+                    cell.dateLabel.textColor = .blue
+                default:
+                    cell.dateLabel.text = "\(numOfDaysInMonth[previousMonth-1]-firstDay+indexPath.row-5)"
+                    print("row : ", indexPath.row)
+                    cell.dateLabel.textColor = .black
+                }
+                cell.dateLabel.font = UIFont.boldSystemFont(ofSize: 10.0)
+                cell.dateLabel.layer.opacity = 0.3
+            }
+            else{
+                switch(indexPath.row % 7){
+                case 0:
+                    cell.dateLabel.text = "\(indexPath.row-5-firstDay-numOfDaysInMonth[self.currentMonth-1])"
+                    cell.dateLabel.textColor = .red
+                case 6:
+                    cell.dateLabel.text = "\(indexPath.row-5-firstDay-numOfDaysInMonth[self.currentMonth-1])"
+                    cell.dateLabel.textColor = .blue
+                default:
+                    cell.dateLabel.text = "\(indexPath.row-5-firstDay-numOfDaysInMonth[self.currentMonth-1])"
+                    cell.dateLabel.textColor = .black
+                }
+                cell.dateLabel.font = UIFont.boldSystemFont(ofSize: 10.0)
+                cell.dateLabel.layer.opacity = 0.3
+            }
+        }
+        cell.dateLabel.textAlignment = .center
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let widthPerItem : CGFloat
+        let heightPerItem : CGFloat
+        if(indexPath.row < 7){
+            //월,화 ... 일
+            let availableWidth = CalendarCollectionView.frame.width
+            widthPerItem = availableWidth / 7 - 1
+            heightPerItem = widthPerItem / 2
+        }
+        else{
+            //날짜
+            let availableWidth = CalendarCollectionView.frame.width
+            widthPerItem = availableWidth / 7 - 1
+            heightPerItem = CalendarCollectionView.frame.height / 7
+        }
+        
+        return CGSize(width: widthPerItem, height: heightPerItem)
+    }
+    
+    
+    //위아래 라인 간격
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+            return 1
+    }
+    //옆 라인 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+            return 1
+    }
+    
+    func slideRight(){
+        if(self.currentMonth == 12){
+            self.currentMonth = 1
+            self.currentYear += 1
+        }
+        else{
+            self.currentMonth += 1
+        }
+        if(self.currentYear % 4 == 0){
+            self.numOfDaysInMonth[1] = 29
+        }
+        else{
+            self.numOfDaysInMonth[1] = 28
+        }
+        self.yearLabel.text = "\(self.currentYear)"
+        self.monthLabel.text = "\(self.currentMonth)월"
+        self.CalendarCollectionView.reloadData()
+    }
+    
+    func slideLeft(){
+        if(self.currentMonth == 1){
+            self.currentMonth = 12
+            self.currentYear -= 1
+        }
+        else{
+            self.currentMonth -= 1
+        }
+        if(self.currentYear % 4 == 0){
+            self.numOfDaysInMonth[1] = 29
+        }
+        else{
+            self.numOfDaysInMonth[1] = 28
+        }
+        self.yearLabel.text = "\(self.currentYear)"
+        self.monthLabel.text = "\(self.currentMonth)월"
+        self.CalendarCollectionView.reloadData()
+    }
+    
+    func deleteAll(){
+        for i in (0..<49){
+            let cell = CalendarCollectionView.dequeueReusableCell(withReuseIdentifier: "CalendarCell", for: IndexPath.init(row: i, section: 0)) as! CalendarCell
+            cell.dateLabel.text = ""
+        }
+    }
+    
+    @IBAction func slideRightBtn(_ sender: Any) {
+//        deleteAll()
+        slideRight()
+    }
+    @IBAction func slideLeftBtn(_ sender: Any) {
+//        deleteAll()
+        slideLeft()
+    }
 }
